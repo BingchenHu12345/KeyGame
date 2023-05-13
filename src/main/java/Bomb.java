4034c139772c11d565d17eb9f79195bd21f79626
bingchen <b@b.com>
2023-05-13 10:39:13 -0700

    Added help screen

---
+++ src/main/java/Bomb.java
@@ -0,0 +1,9 @@
+import com.example.keygame.Bullet;
+
+public class Bomb extends Bullet
+{
+
+    public Bomb(double x, double y, double velX, double velY) {
+        super(x, y, velX, velY);
+    }
+}
